426a84bdd40d69925b3596d2f2c117f70fde1878
DAAK6115 <122787773+DAAK6115@users.noreply.github.com>
2024-09-22 11:42:13 +0000

    Correction de la conversion du nombre de point en entier pour réduire le nombre de point et de place en fonction de la réservation

--- server.py
+++ server.py
@@ -44,30 +44,39 @@ def book(competition, club):
         return redirect(url_for('index'))
 
 @app.route('/purchasePlaces', methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
+
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
     competition_name = request.form['competition']
     club_name = request.form['club']
     places_required = int(request.form['places'])
 
+    # Chargement des compétitions et clubs
     competition = next((comp for comp in competitions if comp['name'] == competition_name), None)
     club = next((c for c in clubs if c['name'] == club_name), None)
 
     if competition and club:
-        if places_required > club['points']:
+        # Conversion des points et des places disponibles en entiers
+        club_points = int(club['points'])
+        competition_places = int(competition['numberOfPlaces'])  # Convertir en entier
+
+        # Vérifications des points et des places disponibles
+        if places_required > club_points:
             flash('Erreur : Vous n\'avez pas assez de points pour réserver autant de places.')
         elif places_required > 12:
             flash('Erreur : Vous ne pouvez pas réserver plus de 12 places par compétition.')
-        elif competition['numberOfPlaces'] < places_required:
+        elif competition_places < places_required:
             flash('Erreur : Il n\'y a pas assez de places disponibles pour cette compétition.')
         else:
-            competition['numberOfPlaces'] -= places_required
-            club['points'] -= places_required  # Déduire les points utilisés
+            # Mise à jour des places disponibles et des points du club
+            competition['numberOfPlaces'] = str(competition_places - places_required)  # Mise à jour des places
+            club['points'] = str(club_points - places_required)  # Mise à jour des points du club
             flash('Réservation complète !')
     else:
         flash('Erreur : Club ou compétition non trouvé.')
-    
-    return render_template('welcome.html', club=club, competitions=competitions)
 
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
